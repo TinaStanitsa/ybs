f07b7927aa4b09b819d80667b023726b4a21a6ea
KONSTANTINA\stani <stanitsakon@gmail.com>
2024-05-05 19:03:01 +0100

    Added navigation component and new fragment

---
+++ YBS/app/src/main/java/com/example/ybsproject/MainFragment.kt
@@ -0,0 +1,31 @@
+package com.example.ybsproject
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.ybsproject.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+
+    // with the backing property of the kotlin we extract
+    // the non null value of the _binding
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
